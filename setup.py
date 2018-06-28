65104eb842b12e2a0a46e0494b20fe781973eae4
Swind <swind@cloudmosa.com>
2018-06-28 14:59:11 +0800

    Bump version: 0.1.0 → 0.1.1-dev

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers = [
 
 setup(
     name='pure-python-adb',
-    version="0.1.0",
+    version="0.1.1-dev",
     description='Pure python implementation of the adb client',
     long_description=readme + '\n\n' + history,
     author='Swind Ou',
