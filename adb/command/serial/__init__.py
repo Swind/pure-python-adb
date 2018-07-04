dd6b12aafa36b74d89c938b29aa3df6914bba0b6
Swind <swind@cloudmosa.com>
2018-07-04 16:14:33 +0800

    1. add 'norebind' parameter to forward 2. add killforward and killforward_all function

--- adb/command/serial/__init__.py
+++ adb/command/serial/__init__.py
@@ -2,24 +2,34 @@ from adb.command import Command
 
 
 class Serial(Command):
-    def _execute_cmd(self, cmd, set_transport):
-        conn = self.create_connection(set_transport=set_transport)
+    def _execute_cmd(self, cmd, with_response=True):
+        conn = self.create_connection(set_transport=False)
 
         with conn:
             conn.send(cmd)
-            result = conn.receive()
-            return result
+            if with_response:
+                result = conn.receive()
+                return result
+            else:
+                conn.check_status()
 
-    def forward(self, local, remote):
-        cmd = "host-serial:{serial}:forward:{local};{remote}".format(
-            serial=self.serial,
-            local=local,
-            remote=remote)
-        self._execute_cmd(cmd, set_transport=False)
+    def forward(self, local, remote, norebind=False):
+        if norebind:
+            cmd = "host-serial:{serial}:forward:norebind:{local};{remote}".format(
+                serial=self.serial,
+                local=local,
+                remote=remote)
+        else:
+            cmd = "host-serial:{serial}:forward:{local};{remote}".format(
+                serial=self.serial,
+                local=local,
+                remote=remote)
+
+        self._execute_cmd(cmd, with_response=False)
 
     def list_forward(self):
         cmd = "host-serial:{serial}:list-forward".format(serial=self.serial)
-        result = self._execute_cmd(cmd, set_transport=False)
+        result = self._execute_cmd(cmd)
 
         forward_map = {}
 
@@ -30,14 +40,22 @@ class Serial(Command):
 
         return forward_map
 
+    def killforward(self, local):
+        cmd = "host-serial:{serial}:killforward:{local}".format(serial=self.serial, local=local)
+        self._execute_cmd(cmd, with_response=False)
+
+    def killforward_all(self):
+        cmd = "host-serial:{serial}:killforward-all".format(serial=self.serial)
+        self._execute_cmd(cmd, with_response=False)
+
     def get_device_path(self):
         cmd = "host-serial:{serial}:get-devpath".format(serial=self.serial)
-        return self._execute_cmd(cmd, set_transport=False)
+        return self._execute_cmd(cmd)
 
     def get_serial_no(self):
         cmd = "host-serial:{serial}:get-serialno".format(serial=self.serial)
-        return self._execute_cmd(cmd, set_transport=False)
+        return self._execute_cmd(cmd)
 
     def get_state(self):
         cmd = "host-serial:{serial}:get-state".format(serial=self.serial)
-        return self._execute_cmd(cmd, set_transport=False)
+        return self._execute_cmd(cmd)
--- test/test_host_serial.py
+++ test/test_host_serial.py
@@ -1,4 +1,5 @@
 import threading
+import pytest
 
 
 def test_shell_sync(device):
@@ -36,3 +37,42 @@ def test_get_serial_no(device):
 def test_get_state(device):
     result = device.get_state()
     assert result == 'device'
+
+
+def test_forward(device):
+    device.forward("tcp:9999", "tcp:7777")
+    forward_list = device.list_forward()
+    assert "tcp:9999" in forward_list
+    assert forward_list['tcp:9999'] == "tcp:7777"
+
+    device.killforward("tcp:9999")
+    forward_list = device.list_forward()
+    assert len(forward_list) == 0
+
+
+def test_forward_killforward_all(device):
+    device.forward("tcp:9999", "tcp:7777")
+    forward_list = device.list_forward()
+    assert "tcp:9999" in forward_list
+    assert forward_list['tcp:9999'] == "tcp:7777"
+
+    device.killforward_all()
+    forward_list = device.list_forward()
+    assert len(forward_list) == 0
+
+
+def test_forward_norebind_failed(device):
+    try:
+        device.forward("tcp:9999", "tcp:7777")
+        forward_list = device.list_forward()
+        assert "tcp:9999" in forward_list
+        assert forward_list['tcp:9999'] == "tcp:7777"
+
+        with pytest.raises(RuntimeError) as excinfo:
+            device.forward("tcp:9999", "tcp:7777", norebind=True)
+
+        assert "cannot rebind existing socket" in str(excinfo.value)
+    finally:
+        device.killforward_all()
+        forward_list = device.list_forward()
+        assert len(forward_list) == 0
