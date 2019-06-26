417539119611f93cf079a7d0e05e575df3e3446e
Swind <swind@cloudmosa.com>
2019-06-26 19:21:05 +0800

    Bump version: 0.1.6-dev → 0.1.7-dev

--- adb/__init__.py
+++ adb/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.6-dev"
+__version__ = "0.1.7-dev"
 
 class InstallError(Exception):
     def __init__(self, path, error):
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ classifiers = [
 
 setup(
     name='pure-python-adb',
-    version="0.1.6-dev",
+    version="0.1.7-dev",
     description='Pure python implementation of the adb client',
     long_description=readme + '\n\n' + history,
     author='Swind Ou',
